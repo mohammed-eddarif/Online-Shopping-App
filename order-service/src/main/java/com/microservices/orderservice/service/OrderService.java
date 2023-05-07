aa511d31cd3c39034ec95ca877c75b4a9d4197ff
mohammed-eddarif <mohammed.eddari06@gmail.com>
2023-05-07 01:20:56 +0100

    Service Discovery Implementation

--- order-service/src/main/java/com/microservices/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservices/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -39,8 +39,8 @@ public class OrderService {
                 .toList();
 
         // call inventory service and place order if product is in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory-service",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory-service",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
