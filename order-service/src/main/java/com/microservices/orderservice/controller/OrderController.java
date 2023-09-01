7da1bbd480c166cb108cfdb703a56a988e5e3c48
mohammed-eddarif <mohammed.eddari06@gmail.com>
2023-09-01 17:43:24 +0100

    keycloak and resilience 4j circuit breaker are added.

--- order-service/src/main/java/com/microservices/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/microservices/orderservice/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.microservices.orderservice.controller;
 
 import com.microservices.orderservice.request.OrderRequest;
 import com.microservices.orderservice.service.impl.OrderServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +17,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderServiceImpl.placeOrder(orderRequest);
         return "Order Passed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops, something went wrong , please try sometime later .";
     }
 }
